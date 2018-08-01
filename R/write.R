eaa4f45114c0250f9dd14876ae9dab03ba644364
btskinner <btskinner@virginia.edu>
2018-08-01 15:14:34 -0400

    fixed error that would write data even if did not pass

--- R/write.R
+++ R/write.R
@@ -61,68 +61,70 @@ write_dua_df <- function(df,
     if (!dua_env[['check_pass']]) {
         messager__(paste0('Data set has not yet passed check. Run ',
                           '-check_dua_restrictions()- to check status.'))
-    }
 
-    ## file extension list
-    fe <- list('rds' = 'rds', 'rdata' = 'rdata', 'csv' = 'csv', 'tsv' = 'tsv',
-               'delimited' = 'txt', 'stata' = 'dta', 'sas' = 'sas7bdat',
-               'spss' = 'sav')
+    } else {
 
-    ## check for file extension
-    ext <- tolower(tools::file_ext(file_name))
-    if (ext == '') {
-        file_name <- paste(file_name, fe[[output_type]], sep = '.')
-    }
+        ## file extension list
+        fe <- list('rds' = 'rds', 'rdata' = 'rdata', 'csv' = 'csv', 'tsv' = 'tsv',
+                   'delimited' = 'txt', 'stata' = 'dta', 'sas' = 'sas7bdat',
+                   'spss' = 'sav')
 
-    ## join name and path
-    f <- file.path(path, file_name)
-
-    ## -----------------------
-    ## write
-    ## -----------------------
-
-    ## arguments
-    args <- list(...)
-
-    ## RDS
-    if (output_type == 'rds') {
-        do.call('saveRDS', c(list('object' = df, 'file' = f), args))
-    }
-
-    ## Rdata
-    if (output_type == 'rdata') {
-        do.call('save', c(list('df', 'file' = f), args))
-    }
-
-    ## delimited files
-    delims <- c('csv','tsv','delimited')
-    if (output_type %in% delims) {
-        if (!'row.names' %in% names(args)) { args[['row.names']] <- FALSE }
-        if (output_type == 'csv') {
-            if (!'sep' %in% names(args)) { args[['sep']] <- ',' }
+        ## check for file extension
+        ext <- tolower(tools::file_ext(file_name))
+        if (ext == '') {
+            file_name <- paste(file_name, fe[[output_type]], sep = '.')
         }
-        if (output_type == 'csv') {
-            if (!'sep' %in% names(args)) { args[['sep']] <- '\t' }
+
+        ## join name and path
+        f <- file.path(path, file_name)
+
+        ## -----------------------
+        ## write
+        ## -----------------------
+
+        ## arguments
+        args <- list(...)
+
+        ## RDS
+        if (output_type == 'rds') {
+            do.call('saveRDS', c(list('object' = df, 'file' = f), args))
         }
-        do.call('write.table', c(list(df, 'file' = f), args))
-    }
 
-    ## Stata
-    if (output_type == 'stata') {
-        fun <- get('write_dta', asNamespace('haven'))
-        do.call(fun, c(list('data' = df, 'path' = f), args))
-    }
+        ## Rdata
+        if (output_type == 'rdata') {
+            do.call('save', c(list('df', 'file' = f), args))
+        }
 
-    ## SAS
-    if (output_type == 'sas') {
-        fun <- get('write_sas', asNamespace('haven'))
-        do.call(fun, c(list('data' = df, 'path' = f)))
-    }
+        ## delimited files
+        delims <- c('csv','tsv','delimited')
+        if (output_type %in% delims) {
+            if (!'row.names' %in% names(args)) { args[['row.names']] <- FALSE }
+            if (output_type == 'csv') {
+                if (!'sep' %in% names(args)) { args[['sep']] <- ',' }
+            }
+            if (output_type == 'csv') {
+                if (!'sep' %in% names(args)) { args[['sep']] <- '\t' }
+            }
+            do.call('write.table', c(list(df, 'file' = f), args))
+        }
 
-    ## SPSS
-    if (output_type == 'spss') {
-        fun <- get('write_sav', asNamespace('haven'))
-        do.call(fun, c(list('data' = df, 'path' = f)))
-    }
+        ## Stata
+        if (output_type == 'stata') {
+            fun <- get('write_dta', asNamespace('haven'))
+            do.call(fun, c(list('data' = df, 'path' = f), args))
+        }
 
+        ## SAS
+        if (output_type == 'sas') {
+            fun <- get('write_sas', asNamespace('haven'))
+            do.call(fun, c(list('data' = df, 'path' = f)))
+        }
+
+        ## SPSS
+        if (output_type == 'spss') {
+            fun <- get('write_sav', asNamespace('haven'))
+            do.call(fun, c(list('data' = df, 'path' = f)))
+        }
+
+    }
 }
